9dae351fb4ef13f596bb2700efc2fead0907ea5f
NguyenTranTrongHuy2912 <23671451.huy@student.iuh.edu.vn>
2026-09-15 03:55:30 +0700

    feat: done Header and BookCard

--- book-store/App.tsx
+++ book-store/App.tsx
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import BookCard from './components/BookCard';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Header />
-      <BookCard/>
-    </View>
+
+      <ScrollView style={styles.contentArea}>
+        <View style={styles.listContainer}>
+          <BookCard
+            title="Sách 1"
+            author="Nguyễn Văn A"
+            price={150000}
+            imageUrl="https://picsum.photos/id/1010/80/110"
+          />
+          <BookCard
+            title="Sách 2"
+            author="Nguyễn Văn A"
+            price={350000}
+            imageUrl="https://picsum.photos/id/1011/80/110"
+          />
+          <BookCard
+            title="Sách 3"
+            author="Nguyễn Văn A"
+            price={85000}
+            imageUrl="https://picsum.photos/id/1003/80/110"
+          />
+          <BookCard
+            title="Sách 4"
+            author="Nguyễn Văn A"
+            price={79000}
+            imageUrl="https://picsum.photos/id/1004/80/110"
+          />
+          <BookCard
+            title="Sách 5"
+            author="Nguyễn Văn A"
+            price={120000}
+            imageUrl="https://picsum.photos/id/1005/80/110"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 1, // Chiếm toàn bộ màn hình điện thoại
+    backgroundColor: '#ffffff',
+    // Dòng dưới giúp UI không bị đè vào thanh trạng thái (tai thỏ, pin, sóng...) trên Android
+    // paddingTop: StatusBar.currentHeight || 0, 
   },
+  contentArea: {
+    flex: 1, 
+    backgroundColor: '#e9ecef', // Màu nền phụ để dễ phân biệt vùng content
+  },
+  listContainer: {
+    padding: 16,
+    gap: 15, // Tạo khoảng cách giữa các Book Card xếp chồng lên nhau
+  }
 });
--- book-store/components/BookCard.tsx
+++ book-store/components/BookCard.tsx
@@ -1,23 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 type BookCardProps = {
-    title: string,
-    author: string,
-    price: number,
-    imageUrl?: string
+  title: string,
+  author: string,
+  price: number,
+  imageUrl?: string
 }
 
-const BookCard = ({title, author, price, imageUrl}:BookCardProps) => {
+const BookCard = ({ title, author, price, imageUrl }: BookCardProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.cover_image}>
-        <Text>Anh Bia</Text>
+        {imageUrl ? (
+          <Image 
+            source={{ uri: imageUrl }} 
+            style={styles.image} 
+            resizeMode="cover"
+          />
+        ) : (
+          <Text style={{ textAlign: 'center', marginTop: 40, fontSize: 12 }}>Ảnh Bìa</Text>
+        )}
       </View>
       <View style={styles.information}>
-        <Text>Ten Sach</Text>
-        <Text>Tac gia</Text>
-        <Text>Gia</Text>
+        <Text numberOfLines={2} style={styles.titleText}>{title}</Text>
+        <Text>{author}</Text>
+        <Text style={styles.priceText}>{price} đ</Text>
       </View>
     </View>
   )
@@ -26,27 +34,39 @@ const BookCard = ({title, author, price, imageUrl}:BookCardProps) => {
 export default BookCard
 
 const styles = StyleSheet.create({
-    container:{
-        display: 'flex',
-        flexDirection: 'row',
-        width: '100%',
-        backgroundColor:'red',
-        padding:5,
-        gap: 10
-    },
-    cover_image:{
-        height: 110,
-        width: 80,
-        backgroundColor: 'yellow',
-        borderWidth: 1,
-        borderRadius: 5,
-    },
-    information:{
-        flex: 1,
-        flexDirection: 'column',
-        gap: 5,
-        justifyContent: 'space-around'
-        
-    }
-
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start', 
+    width: '100%',
+    backgroundColor: '#f5f5f5', 
+    padding: 10,
+    gap: 10
+  },
+  cover_image: {
+    height: 110,
+    width: 80,
+    backgroundColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    overflow: 'hidden',
+  },
+  information: {
+    flex: 1,
+    height: 110,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  titleText: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  priceText: {
+    color: 'green',
+    fontWeight: 'bold'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
 })
