e1c23507f23664a74609f99f1606ca8b4c2f45ca
NguyenTranTrongHuy2912 <23671451.huy@student.iuh.edu.vn>
2026-09-15 04:39:31 +0700

    feat: add CategoryChips component to display book categories

---
+++ book-store/components/CategoryChips.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CATEGORIES = [
+    'Văn học', 'Kinh tế', 'Thiếu nhi',
+    'Truyện tranh', 'Ngoại ngữ', 'Lịch sử'
+];
+
+const CategoryChips = () => {
+    return (
+        <View style={styles.wrapper}>
+            <View style={styles.container}>
+                {CATEGORIES.map((item, index) => (
+                    <View key={index} style={styles.chip}>
+                        <Text style={styles.chipText}>{item}</Text>
+                    </View>
+                ))}
+            </View>
+        </View>
+    );
+}
+
+export default CategoryChips
+
+const styles = StyleSheet.create({
+    wrapper: {
+        padding: 16,
+    },
+    container: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: 8,
+        backgroundColor: '#f5f5f5',
+        padding: 16,
+        borderWidth: 2,
+        borderColor: '#b3b3ff',
+        borderStyle: 'dashed',
+
+    },
+    chip: {
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: 'indigo',
+        backgroundColor: '#ffffff',
+    },
+    chipText: {
+        color: '#333',
+    }
+})
